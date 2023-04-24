1dbdee8e0b7d5a3bbc71c43d9de292a5d61c2041
dark-king-001 <piyushat115@gmail.com>
2023-04-25 00:16:51 +0530

    Added guide comments

--- test.py
+++ test.py
@@ -19,19 +19,24 @@ model.add(Dense(1, activation='sigmoid')) # Output layer with 1 neuron
 
 # Compiling the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# %% Training the model
+# %% 
+# manual training data
 X_train = np.array([[[0,1,0,1]],[[0,0,1,1]]])
 y_train = np.array([[0],[1]])
-# %%
+# %% Training the model
 history = model.fit(X_train, y_train, epochs=1000, batch_size=32)
 plt.plot(history.history['loss'], label='Training Loss')
 plt.legend()
 plt.show()
 #%%
+# loading test data
 X_test = np.array([[[0,1,0,1]],[[0,0,1,1]]])
 y_test = np.array([[0],[1]])
+# evaluating model
 score = model.evaluate(X_test, y_test, batch_size=32)
 print("Accuracy: %.2f%%" % (score[1]*100))
+
+# printing pridiction score
 y_pred = model.predict(X_test)
 plt.plot(y_test, label='Actual')
 plt.plot(y_pred, label='Predicted')
