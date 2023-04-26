3a1b6ba97471d99b2dd402ce1421dacd63076b66
dark-king-001 <piyushat115@gmail.com>
2023-04-26 14:53:22 +0530

    create a function and improved flow

--- load_model.py
+++ load_model.py
@@ -1,28 +1,21 @@
-# %% activating conda enviornment
-import os
-os.system("conda activate tf")
-# %% Activating tensorflow
-import tensorflow as tf 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-# %% importing required packages
+# %% loading keras LSTM and other libs
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, RNN
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# %% importing additional module
-from statsmodels.tsa.seasonal import seasonal_decompose
-
-file = open('model.txt','rb')
-# %% loading sklearn
-from sklearn.preprocessing import MinMaxScaler
-# %% loading kera timeseries generator
-from keras.preprocessing.sequence import TimeseriesGenerator
-
-# %% loading keras LSTM and other libs
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-# %% Load the pickled model
 import pickle
-with open('model', 'rb') as file:
-    response = pickle.load(file)
+import tensorflow as tf 
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+#%%
+import pickle
+def loadmodel():
+    with open(input('Enter model name : '), 'rb') as file:
+        return pickle.load(file)
+model = loadmodel()
+# %%
+# %%
+pred_data = np.array([[[50],[4000],[40],[1.0]]])
+y_pred = model.predict(pred_data)*4
+y_pred
 # %%
