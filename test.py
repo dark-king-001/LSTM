20f2c22520cc041eb20d886c045e04c6ffbb7a60
dark-king-001 <piyushat115@gmail.com>
2023-04-28 13:01:56 +0530

    simple python version of preprocessing directive

--- test.py
+++ test.py
@@ -1,94 +1,42 @@
-# %% loading keras LSTM and other libs
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, RNN
-import pandas as pd
+# %%
 import numpy as np
-import matplotlib.pyplot as plt
-import pickle
-import tensorflow as tf 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
-# %% 
-# manual training data
-data = pd.read_csv('datasets/graduation_rate.csv')
-data1 = np.array(data['ACT composite score'])
-data2 = np.array(data['SAT total score'])
-data3 = np.array(data['parental income'])
-data4 = np.array(data['high school gpa'])
-data5 = np.array(data['college gpa'])
-data6 = np.array(data['years to graduate'])
+from sklearn.feature_extraction.text import TfidfVectorizer
 # %%
-def segmentation(start,end):
-    X = []
-    y = []
-    for i in range(start,end):
-        segment = [[data1[i]],[data2[i]],[data3[i]],[data4[i]],[data5[i]]]
-        X.append(segment)
-        y.append([(data6[i])])
-    return np.array(X),np.array(y)
+
+# Example list of documents
+documents = [
+    'Emotional Rescue is the 15th British and 17th American studio album by English rock band the Rolling Stones, released on 20 June 1980 by Rolling Stones Records. Following the success of their previous album, Some Girls, their biggest hit to date, the Rolling Stones returned to the studio in early 1979 to start writing and recording its follow-up. Full-time members Mick Jagger (vocals), Keith Richards (guitar), Ronnie Wood (guitar), Bill Wyman (bass) and Charlie Watts (drums) were joined by frequent collaborators Ian Stewart (keyboards), Nicky Hopkins (keyboards), Bobby Keys (saxophone) and Sugar Blue (harmonica).Upon release, the album topped the charts in at least six countries, including the United States, UK, and Canada. Hit singles from it include the title track, which reached No. 1 in Canada, No. 3 in the United States, and No. 9 in the UK and a top-40 single in several countries. The recording sessions for Emotional Rescue were so productive that several tracks left off the album would form the core of the follow-up, 1981s Tattoo You.',
+    'This is the second second document.',
+    'And the third one.',
+    'Is this the first document?',
+]
+# %%
+ids = np.array(documents[0].split(' '))
+# %%
+print(len(ids))
+# %%
+# Initialize a TfidfVectorizer object with custom options
+vectorizer = TfidfVectorizer(max_features=50000)
+# Fit and transform the documents into a TF-IDF matrix
+tfidf_matrix = vectorizer.fit_transform(ids)
+# %%
+print(tfidf_matrix[1][0])
 
 # %%
-segment_length = len(data)
-divide = int(len(data)*0.8)
-X_train , y_train = segmentation(0,divide)
-X_test , y_test = segmentation(divide,len(data))
-
-print(X_train.shape)
-# print(len(X_train))
-# print(y_train)
-# print(len(y_train))
-# X_train = np.array([[[0,1,0,1]],[[0,0,1,1]]])
-# print(X_train.shape)
-# y_train = np.array([[0],[1]])
-# print(y_train.shape)
-
-# %% Creating the neural network model
-model = Sequential()
-
-# Adding the layers to the model
-model.add(LSTM(64, input_shape=(5,1))) # LSTM layer with 8 neurons, input shape of (1, 4)
-model.add(Dense(32, activation='relu')) # Hidden layer with neurons
-model.add(Dense(32, activation='relu')) # Hidden layer with neurons
-model.add(Dense(32, activation='relu')) # Hidden layer with neurons
-model.add(Dense(32, activation='relu')) # Hidden layer with neurons
-model.add(Dense(32, activation='relu')) # Hidden layer with neurons
-model.add(Dense(32, activation='relu')) # Hidden layer with neurons
-model.add(Dense(1, activation='linear')) # Output layer with 1 neuron
-
-linear_loss ='mean_squared_error'
-sigmoid_loss = 'binary_crossentropy'
-
-# Compiling the model
-model.compile(loss=linear_loss, optimizer='RMSprop', metrics=['accuracy'])
-# %% Training the model
-history = model.fit(X_train, y_train, epochs=50, batch_size=5)
-plt.plot(history.history['loss'], label='Training Loss')
-plt.legend()
-plt.show()
-# %%
-# loading test data
-# X_test = np.array([[[0,1,0,1]],[[0,0,1,1]]])
-# y_test = np.array([[0],[1]])
-# evaluating model
-score = model.evaluate(X_test, y_test, batch_size=1)
-print("Accuracy: %.2f%%" % (score[0]*100))
-# %%
-# printing pridiction score
-y_pred = model.predict(X_test)
-plt.plot(y_test, label='Actual')
-plt.plot(y_pred, label='Predicted')
-plt.legend()
-plt.show()
-# %%
-pred_data = np.array([[[0],[0],[0],[4.0]]])
-y_pred = model.predict(pred_data)
-y_pred
-# %%
-# Save the trained model as a pickle string.
-saved_model = pickle.dumps(model)
-# %%
-saved_model
-# %%
-file = open(input("enter name of model:"),"wb")
-file.write(saved_model)
+# Retrieve the vocabulary
+vocabulary = vectorizer.get_feature_names_out()
+idf = vectorizer.idf_
+tf_matrix = tfidf_matrix / np.reshape(idf, (1, -1))
+# Retrieve the original text with stop words included
+original_text = []
+for i in range(len(ids)):
+    row = np.asarray(tf_matrix[i,:]).squeeze()
+    words = []
+    for j in np.where(row > 0)[0]:
+        words.append(vocabulary[j])
+    text = ' '.join(words)
+    original_text.append(text)
+# Reconstruct the original text
+for text in original_text:
+    print(text)
 # %%
